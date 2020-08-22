c3a0774730cac39019ecae099ae20161d474003d
MuellerMarius <marius_m@gmx.net>
2020-08-22 19:39:30 +0000

    Updates

---
+++ precache-manifest.d98cb980c9b0ceb25fb50f14c992fdb4.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "80ff36da0b58989774af",
+    "url": "/tetrisudoku/static/js/main.80ff36da.chunk.js"
+  },
+  {
+    "revision": "7a56ceaa52c340f4b3e9",
+    "url": "/tetrisudoku/static/js/1.7a56ceaa.chunk.js"
+  },
+  {
+    "revision": "b1a0ebb6dfe28c25be32",
+    "url": "/tetrisudoku/static/js/runtime~main.b1a0ebb6.js"
+  },
+  {
+    "revision": "bbf20ead52ce3535d3483656ea3df0d7",
+    "url": "/tetrisudoku/index.html"
+  }
+];
